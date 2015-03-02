1eea3746706438a68b55fe54994a0e3ed8f4f653
demievil <liuyanbupt@163.com>
2015-03-02 19:49:59 +0800

    Revert "modify Toast utils"
    This reverts commit 0cfc454f1f5ac724de10c2c361700f777f0b5783.

--- app/src/main/java/com/bupt/booktrade/utils/ToastUtils.java
+++ app/src/main/java/com/bupt/booktrade/utils/ToastUtils.java
@@ -4,42 +4,30 @@ import android.content.Context;
 import android.widget.Toast;
 
 
-public class ToastUtils extends Toast {
+public class ToastUtils{
+	
+	private static Toast mToast;
 
-    /**
-     * Construct an empty Toast object.  You must call {@link #setView} before you
-     * can call {@link #show}.
-     *
-     * @param context The context to use.  Usually your {@link android.app.Application}
-     *                or {@link android.app.Activity} object.
-     */
-    public ToastUtils(Context context) {
-        super(context);
-    }
+	public static void showToast(Context context, int msg, int duration) {
+	
+		if (mToast == null) {
+			mToast = Toast.makeText(context, msg, duration);
+		} else {
+			mToast.setText(msg);
+		}
+		mToast.show();
+	}
+    public static void showToast(Context context, String msg, int duration) {
 
-    private static Toast mToast;
-
-
-    public static void showToast(Context context, int resId, int duration) {
         if (mToast == null) {
-            mToast = Toast.makeText(context, resId, duration);
+            mToast = Toast.makeText(context, msg, duration);
         } else {
-            mToast.setText(resId);
+            mToast.setText(msg);
         }
         mToast.show();
     }
-
-    public static void showToast(Context context, String text, int duration) {
-        if (mToast == null) {
-            mToast = Toast.makeText(context, text, duration);
-        } else {
-            mToast.setText(text);
-        }
-        mToast.show();
-    }
-
-    public static void clearToast() {
-        mToast.cancel();
-    }
+	public static void clearToast(){
+		mToast.cancel();
+	}
 }
 
