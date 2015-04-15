2fc1c3a66ec09edcddc6a9b86f9c121cc7db47f8
demievil <liuyanbupt@163.com>
2015-04-15 21:26:26 +0800

    add copyright

--- app/src/main/java/com/bupt/booktrade/MyApplication.java
+++ app/src/main/java/com/bupt/booktrade/MyApplication.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2015 Liu Yan
+ * Email:liuyanbupt@163.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
 package com.bupt.booktrade;
 
 import android.app.Activity;
@@ -10,10 +29,6 @@ import com.bupt.booktrade.utils.Constant;
 import cn.bmob.v3.Bmob;
 import cn.bmob.v3.BmobUser;
 
-
-/**
- * Created by LiuYan on 2015/3/17.
- */
 public class MyApplication extends Application {
     public static final String TAG = "MyApplication";
     public static MyApplication myApplication = null;
